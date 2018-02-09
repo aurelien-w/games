4cb2e1cfcc77b8dceeb7a0968e813a44c863a8e6
NicolasMica <nicolas@micallef.pro>
2018-02-09 23:33:51 +0100

    Added GameController CRUD methods

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -8,6 +8,66 @@ use Illuminate\Http\Request;
 class GameController extends Controller
 {
     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Game::with('player_a', 'player_b')->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return Game
+     */
+    public function store(Request $request)
+    {
+        $data = $this->validates($request);
+
+        return Game::create($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $game
+     * @return Game|\Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model
+     */
+    public function show($game)
+    {
+        return Game::with('player_a', 'player_b')->findOrFail($game);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Models\Game $game
+     * @return bool
+     */
+    public function update(Request $request, Game $game)
+    {
+        $data = $this->validates($request);
+
+        return $game->update($data);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Game $game
+     * @return bool|null
+     * @throws \Exception
+     */
+    public function destroy(Game $game)
+    {
+        return $game->delete();
+    }
+
+    /**
      * Game validation rules
      * @param Request $request
      * @return mixed - Validated data array
