c428525b3b62d1a27b19c1e7117aa1abc32fe330
NicolasMica <nicolas@micallef.pro>
2018-02-09 23:38:19 +0100

    Added player _R__ methods

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -7,4 +7,24 @@ use Illuminate\Http\Request;
 
 class PlayerController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Player::with('game_a', 'game_b')->get();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $player
+     * @return Player|\Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model
+     */
+    public function show($player)
+    {
+        return Player::with('game_a', 'game_b')->findOrFail($player);
+    }
 }
