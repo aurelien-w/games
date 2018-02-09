f50aaa40b757516956a5676e78d1c9d72eda1904
NicolasMica <nicolas@micallef.pro>
2018-02-09 23:33:34 +0100

    Added GameController validation

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -7,4 +7,17 @@ use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
+    /**
+     * Game validation rules
+     * @param Request $request
+     * @return mixed - Validated data array
+     */
+    protected function validates(Request $request) {
+        return $request->validate([
+            'player_a' => 'required|different:player_b|exists:players,id',
+            'player_b' => 'required|different:player_a|exists:players,id',
+            'score_a'  => 'required|numeric|min:0',
+            'score_b'  => 'required|numeric|min:0'
+        ]);
+    }
 }
