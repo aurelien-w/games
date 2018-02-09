4737fdd20dfb8a82a38577e211aa972af33099bc
NicolasMica <nicolas@micallef.pro>
2018-02-09 21:34:54 +0100

    Updated mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+let mix = require('laravel-mix')
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +11,14 @@ let mix = require('laravel-mix');
  |
  */
 
+mix.options({})
+    .autoload({})
+
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+
+if (mix.inProduction()) {
+    mix.version()
+} else {
+    mix.sourceMaps()
+}
