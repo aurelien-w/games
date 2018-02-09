2bf5c27464f9733f980a979733daaab27945dcb1
NicolasMica <nicolas@micallef.pro>
2018-02-09 23:48:54 +0100

    Added axios depencendy & config

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,15 @@
 import Vue from 'vue'
+import axios from 'axios'
+
+window.axios = axios.create({
+    baseURL: window.location.origin,
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]').content,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+})
 
 import App from './components/App.vue'
 
