9d4f441dd81a76f0a505d3dcd346f205cd0806e5
NicolasMica <nicolas@micallef.pro>
2018-02-10 18:21:32 +0100

    Updated default page title format

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', config('app.name'))</title>
+    <title>@yield('title', strtoupper(config('app.name')))</title>
 
     @section('styles')
         <link href="https://fonts.googleapis.com/css?family=Poppins:400,700" rel="stylesheet">
