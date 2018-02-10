07cb6b9d28564564622ccbc125084fd0e24f963a
NicolasMica <nicolas@micallef.pro>
2018-02-10 13:12:44 +0100

    Added loading mixin

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import axios from 'axios'
 
 import router from './routes'
+
+import App from './components/App.vue'
+
 window.axios = axios.create({
-    baseURL: window.location.origin,
+    baseURL: window.location.origin + '/api/',
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
         'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
@@ -12,11 +15,25 @@ window.axios = axios.create({
     }
 })
 
-import App from './components/App.vue'
+Vue.mixin({
+    data () {
+        return {
+            loading: false
+        }
+    },
+    methods: {
+        /**
+         * Toggles loading state
+         */
+        toggleLoading() {
+            this.loading = !this.loading
+        }
+    }
+})
 
 new Vue({
     el: '#app',
     components: { App },
-    template: '<App/>'
+    template: '<App/>',
     router
 })
