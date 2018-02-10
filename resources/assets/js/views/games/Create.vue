e4f54e3946aeeb206e3764ee527110bf4391b93e
NicolasMica <nicolas@micallef.pro>
2018-02-10 19:31:02 +0100

    First game form impl.

---
+++ resources/assets/js/views/games/Create.vue
@@ -0,0 +1,132 @@
+<template>
+    <div class="box">
+        <form method="POST" @submit.prevent="submit">
+            <!-- PLAYER A -->
+            <div class="columns">
+                <div class="column">
+                    <div class="field">
+                        <label for="player_a" class="label">Joueur A</label>
+                        <div class="control is-expanded has-icons-left">
+                            <span class="icon is-left">
+                                <i class="fal fa-user"></i>
+                            </span>
+                            <div class="select is-fullwidth">
+                                <select id="player_a" v-model="player_a">
+                                    <option value="" selected disabled>Choisir un joueur</option>
+                                    <option :value="player.id" v-for="player in playersA.all()">{{ player.name }}</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column is-narrow">
+                    <div class="field">
+                        <label for="score_a" class="label">Score A</label>
+                        <div class="control has-icons-left">
+                            <span class="icon is-left">
+                                <i class="fas fa-futbol"></i>
+                            </span>
+                            <input type="number" id="score_a" class="input" v-model="score_a">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- PLAYER B -->
+            <div class="columns">
+                <div class="column">
+                    <div class="field">
+                        <label for="player_b" class="label">Joueur B</label>
+                        <div class="control is-expanded has-icons-left">
+                            <span class="icon is-left">
+                                <i class="fal fa-user"></i>
+                            </span>
+                            <div class="select is-fullwidth">
+                                <select id="player_b" v-model="player_b">
+                                    <option value="" selected disabled>Choisir un joueur</option>
+                                    <option :value="player.id" v-for="player in playersB.all()">{{ player.name }}</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column is-narrow">
+                    <div class="field">
+                        <label for="score_a" class="label">Score B</label>
+                        <div class="control has-icons-left">
+                            <span class="icon is-left">
+                                <i class="fas fa-futbol"></i>
+                            </span>
+                            <input type="number" id="score_b" class="input" v-model="score_b">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- BUTTONS -->
+            <div class="level is-mobile">
+                <div class="level-left">
+                    <button class="button is-danger is-outlined is-size-7-touch" @click="$emit('close')">
+                        <span class="icon">
+                            <i class="fal fa-times"></i>
+                        </span>
+                        <span>Annuler</span>
+                    </button>
+                </div>
+                <div class="level-right">
+                    <button type="submit" class="button is-success is-outlined is-size-7-touch">
+                        <span>Sauvegarder</span>
+                        <span class="icon">
+                            <i class="fal fa-check"></i>
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    import Vuex from 'vuex'
+
+    export default {
+        name: 'GamesCreate',
+        data () {
+            return {
+                player_a: null,
+                player_b: null,
+                score_a: null,
+                score_b: null
+            }
+        },
+        computed: {
+            ...Vuex.mapGetters(['players']),
+
+            playersA () {
+                if (this.player_b === null) return this.players
+
+                return this.players.except(
+                    this.players.first(player => player.id === this.player_b)
+                )
+            },
+
+            playersB () {
+                if (this.player_a === null) return this.players
+
+                return this.players.except(
+                    this.players.first(player => player.id === this.player_a)
+                )
+            }
+        },
+        methods: {
+            ...Vuex.mapActions(['fetchPlayers']),
+
+            submit (event) {
+
+            }
+        },
+        created () {
+            this.toggleLoading()
+            this.fetchPlayers()
+                .then(this.toggleLoading)
+        }
+    }
+</script>
