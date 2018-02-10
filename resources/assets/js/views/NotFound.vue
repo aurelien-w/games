06c72b535ba1ce0ce70fc89c701851954d4a1e4e
NicolasMica <nicolas@micallef.pro>
2018-02-10 13:11:35 +0100

    Added 404 view component

---
+++ resources/assets/js/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="section">
+        <div class="container has-text-centered">
+            <p class="title is-1">
+                404
+            </p>
+            <p class="subtitle is-3">
+                <span class="icon is-large">
+                    <i class="fal fa-exclamation-triangle"></i>
+                </span>
+                <span>Page introuvable</span>
+            </p>
+            <img src="https://media.giphy.com/media/UtLNNNlNKFU3K/giphy.gif" alt="404">
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'NotFound'
+    }
+</script>
