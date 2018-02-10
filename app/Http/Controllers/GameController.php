214c544085b360c1d533d8dec31ab17d940bab17
NicolasMica <nicolas@micallef.pro>
2018-02-10 22:19:26 +0100

    Fixed wrong table name ('cause of wrong naming)

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -74,8 +74,8 @@ class GameController extends Controller
      */
     protected function validates(Request $request) {
         return $request->validate([
-            'player_a' => 'required|different:player_b|exists:players,id',
-            'player_b' => 'required|different:player_a|exists:players,id',
+            'player_a' => 'required|different:player_b|exists:player,id',
+            'player_b' => 'required|different:player_a|exists:player,id',
             'score_a'  => 'required|numeric|min:0',
             'score_b'  => 'required|numeric|min:0'
         ]);
