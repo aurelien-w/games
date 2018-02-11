a83566c7789b909c4fecdce0ef42bf5d2ffb549f
NicolasMica <nicolas@micallef.pro>
2018-02-11 01:54:15 +0100

    Added flash helper

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,6 +3,27 @@ import Vue from 'vue'
 import router from './routes'
 import store from './store'
 
+// Helpers
+window.Event = new Vue
+window.flash = function (message, options = {}) {
+    options = {
+        type: 'is-danger',
+        icon: 'fas fa-exclamation-triangle',
+        channel: 'flash',
+        fixed: {
+            top: true,
+            right: true,
+            bottom: false,
+            left: false
+        },
+        timeout: 3000,
+        ...options
+    }
+
+    window.Event.$emit('flash', { ...options, message })
+}
+
+// Components
 import App from './components/App'
 import Modal from './components/Modal'
 import Flash from './components/Flash'
